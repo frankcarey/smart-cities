2e97b2ba6ef7816389b0c0ac7d7d9dc3a75eb247
Frank Carey <frank@devinci.io>
2017-03-25 12:58:37 -0400

    Activate before installing.

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 pip install pip --upgrade
 pip install virtualenv
 virtualenv --distribute --no-site-packages .env
+. .env/bin/activate
 pip install -r requirements.txt
+
+echo "======="
+echo " run the following on the command line to activate the environment.:"
+echo "    source .env/bin/activate"
+echo "======="
